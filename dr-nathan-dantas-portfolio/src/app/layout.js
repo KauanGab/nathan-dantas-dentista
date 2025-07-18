ae65daefd0536647eab9b9b7f2e6b8984674ac78
KauanGab <auansozi@gmail.com>
2025-07-18 12:59:23 -0300

    Feat:adicionando logo na aba do navegador

--- dr-nathan-dantas-portfolio/src/app/layout.js
+++ dr-nathan-dantas-portfolio/src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Dr. Nathan Dantas - Transformando Sorrisos",
+  title: "Dr. Nathan Dantas – Seu Sorriso, Nossa Missão",
   description: "Site portfólio do Dr. Nathan Dantas, dentista especializado em transformar sorrisos para uma melhor qualidade de vida.",
 };
 
