5ee6c630b2cc400551342c3e2b0250240b8aba3e
KauanGab <auansozi@gmail.com>
2025-07-18 12:49:34 -0300

    FEat: adicioanndo icones dos clientes

--- dr-nathan-dantas-portfolio/src/app/components/TestimonialsSection.js
+++ dr-nathan-dantas-portfolio/src/app/components/TestimonialsSection.js
@@ -3,6 +3,13 @@
 import React from 'react';
 import Image from 'next/image';
 
+import avatar1 from '../assets/avatar-1.jpeg';
+import avatar2 from '../assets/avatar-2.jpg';
+import avatar3 from '../assets/avatar-3.jpg';
+import avatar4 from '../assets/avatar-4.jpg';
+import avatar5 from '../assets/avatar-5.jpg';
+import avatar6 from '../assets/avatar-6.jpg';
+
 const StarIcon = ({ className }) => (
   <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
@@ -13,32 +20,32 @@ const testimonials = [
   {
     quote: "O Dr. Nathan é um excelente profissional! Atencioso e muito competente. Meu sorriso nunca esteve tão bonito e saudável.",
     author: "Ana Silva",
-    avatar: "/avatars/avatar-1.jpg",
+    avatar: avatar1,
   },
   {
     quote: "Atendimento impecável e resultados surpreendentes no meu clareamento. A clínica é moderna e a equipe é fantástica. Recomendo!",
-    author: "Carlos Souza",
-    avatar: "/avatars/avatar-2.jpg",
+    author: "Juliana Pereira",
+    avatar: avatar2,
   },
   {
     quote: "Tinha muito medo de dentista, mas o Dr. Nathan e sua equipe me deixaram super à vontade. O tratamento foi tranquilo e indolor.",
     author: "Mariana Costa",
-    avatar: "/avatars/avatar-3.jpg",
+    avatar: avatar3,
   },
   {
     quote: "Fiz a extração do siso e a recuperação foi muito mais rápida do que eu esperava. Profissionalismo nota mil!",
     author: "Pedro Oliveira",
-    avatar: "/avatars/avatar-4.jpg",
+    avatar: avatar4,
   },
   {
     quote: "O resultado das minhas facetas de resina ficou incrível, super natural. Transformou completamente meu sorriso. Estou muito feliz!",
-    author: "Juliana Pereira",
-    avatar: "/avatars/avatar-5.jpg",
+    author: "Carlos Souza",
+    avatar: avatar5,
   },
   {
     quote: "Serviço de emergência ágil e eficiente. Fui atendido rapidamente e resolveram meu problema com muita atenção e cuidado.",
     author: "Ricardo Lima",
-    avatar: "/avatars/avatar-6.jpg",
+    avatar: avatar6,
   },
 ];
 
